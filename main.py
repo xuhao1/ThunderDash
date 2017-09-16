ee8f3dac1691a303d0736f25562612c9ff1658c2
xuhao1 <xuhao3e8@gmail.com>
2017-09-16 22:30:31 +0800

    Test on mobile phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from pymavlink import mavutil
-from pymavlink.quaternion import  *
+from pymavlink.quaternion import *
 import sys
-import win32api,win32con
 import math
 import pyvjoy
 import threading
@@ -9,88 +8,112 @@ from proxy_8111 import proxy_8111
 from wt_server import *
 from util import *
 import pygame
+from pynput.keyboard import Key, Listener
+
 
 class game_aircraft_control:
     def __init__(self):
+        pygame.init()
         pygame.joystick.init()
+
         self.t16000m = None
+        self.master = None
+        self.cmdpov = None
+
         for k in range(pygame.joystick.get_count()):
             if pygame.joystick.Joystick(k).get_name() == "T.16000M":
                 self.t16000m = pygame.joystick.Joystick(k)
+        if self.t16000m is None:
+            print("No t16000m found")
+        else:
+            self.t16000m.init()
+            print("Found Joystick {0}".format(self.t16000m.get_name()))
 
+        self.q_base = None
+        self.vjoy = pyvjoy.VJoyDevice(1)
+        self.proxy_8111_worker = proxy_8111()
 
-master = mavutil.mavlink_connection("COM3", baud="230400")
+        self.pov_x_offset = 0
+        self.pov_y_offset = 0
 
-global q_base
-q_base = None
+        try:
+            self.master = mavutil.mavlink_connection("COM3", baud="230400")
+            print("Link to COM3 with mavlink")
+        except:
+            print("No master Found")
+            self.master = None
 
-global joy
-joy = pyvjoy.VJoyDevice(1)
+    def on_press(self, key):
+        if key == Key.f9:
+            print("reset joystick pov")
+            self.q_base = None
 
-from pynput.keyboard import Key, Listener
+    def set_pov(self, x, y):
+        self.vjoy.set_cont_pov(2, toHexCmd(0.1 + y + self.pov_y_offset))
+        self.vjoy.set_cont_pov(1, toHexCmd(0.1 + x + self.pov_x_offset))
 
-def on_press(key):
-    if key == Key.f9:
-        print("reset joystick pov")
-        global q_base
-        q_base = None
-
-def on_release(key):
-    pass
-
-
-global cmdxyz
-cmdxyz = None
-def proccess_messages(m):
-    '''show incoming mavlink messages'''
-    print("Starting recieve data")
-    while True:
-        msg = m.recv_match(blocking=True)
+    def update_from_mavlink(self):
+        if self.master is None:
+            return
+        msg = self.master.recv_match(blocking=True)
         if not msg:
-            continue
+            return
         if msg.get_type() == "BAD_DATA":
-            if mavutil.all_printable(msg.data):
-                #sys.stdout.write(msg.data)
-                sys.stdout.flush()
+            return
         else:
             if msg._type == "ATTITUDE_QUATERNION":
-                q_now = QuaternionBase(attitude=[msg.q1, msg.q2, msg.q3, msg.q4])
-                global q_base
-                if q_base is None:
-                    q_base = q_now.inversed
-                q_now = q_base.__mul__(q_now)
-                eul = q_now.euler * 180 / 3.14
-                global cmdxyz
-                if cmdxyz is None:
-                    cmdxyz = eul / 180
-                else:
-                    cmdxyz = eul / 180 * 0.3 + cmdxyz * 0.7
-                #win32api.mouse_event(win32con.)
-                #print(eul)
-                global joy
-                #print(cmdxyz*100)
-                joy.set_cont_pov(2, toHexCmd(0.1+cmdxyz[1]))
-                joy.set_cont_pov(1, toHexCmd(0.1+cmdxyz[0]))
-                #
+                quat = [msg.q1, msg.q2, msg.q3, msg.q4]
+                self.process_pov_quat(quat)
+
+    def process_pov_quat(self,quat):
+        q_now = QuaternionBase(attitude=quat)
+        if self.q_base is None:
+            self.q_base = q_now.inversed
+        q_now = self.q_base.__mul__(q_now)
+        eul = q_now.euler* 180 / 3.14
+        #print(eul)
+        #print(eul)
+        if self.cmdpov is None:
+            self.cmdpov = eul / 180
+        else:
+            # A naive lowpass filter
+            self.cmdpov = eul / 180 * 0.4 + self.cmdpov * 0.6
+        self.set_pov(-self.cmdpov[2], self.cmdpov[1])
+
+    def update_from_external_joy(self):
+        pygame.event.get()
+        if self.t16000m is None:
+            return
+        # print(self.t16000m.get_hat(0))
+        # print(self.t16000m.get_axis(0))
+        self.pov_x_offset, self.pov_y_offset = self.t16000m.get_hat(0)
+        self.pov_x_offset = -self.pov_x_offset *0.2
+        self.pov_y_offset = self.pov_y_offset *0.2
+
+    def main_thread(self):
+        if not (self.master is None):
+            print("recieve master")
+            self.master.recv()
+        while True:
+            #print("refresh")
+            if not (self.master is None):
+                self.update_from_mavlink()
+            else:
+                time.sleep(0.01)
+            self.update_from_external_joy()
+
+    def run(self):
+        print("Run....")
+        self.th0 = threading.Thread(target=self.proxy_8111_worker.run)
+        self.th1 = threading.Thread(target=lambda: start_wt_server(self))
+        self.th2 = threading.Thread(target=self.main_thread)
+        self.th0.start()
+        self.th1.start()
+        self.th2.start()
+        with Listener(
+                on_press=lambda x: self.on_press(x)) as listener:
+            listener.join()
 
 
-# wait for the heartbeat msg to find the system ID
-# wait_heartbeat(master)
-# master.wait_heartbeat()
-
-
-master.recv()
-
-proxy_8111_worker = proxy_8111()
-t = threading.Thread(target=lambda :proccess_messages(master))
-t1 = threading.Thread(target= proxy_8111_worker.run)
-t2 = threading.Thread(target= lambda: start_wt_server(proxy_8111_worker,joy))
-
-t.start()
-t1.start()
-t2.start()
-
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+aircraft_con = game_aircraft_control()
+aircraft_con.run()
--- wt_server.py
+++ wt_server.py
@@ -54,14 +54,40 @@ class WTControlSocket(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
 
-def make_app(worker,joy):
+class WTPOVSocket(tornado.websocket.WebSocketHandler):
+
+    def initialize(self, aircraft_con):
+        self.aircraft_con = aircraft_con
+        print("Init Attitude handler")
+
+    def open(self):
+        print("WS Attitude Online")
+
+    def on_message(self, message):
+        try:
+            data = json.loads(message)
+            if self.aircraft_con.master is None:
+                self.aircraft_con.process_pov_quat(data["quat"])
+            #print(data["quat"])
+        except Exception as e:
+            print("Recieve wrong message {0}".format(e))
+
+    def on_close(self):
+        print("Con WebSocket closed")
+
+    def check_origin(self, origin):
+        return True
+def make_app(worker,joy,aircraft_con):
     return tornado.web.Application([
         (r"/flight_info", WTWebSocket,{'worker':worker}),
         (r"/flight_control", WTControlSocket, {'joy': joy}),
+        (r"/pov", WTPOVSocket, {'aircraft_con': aircraft_con}),
         (r'/(.*)', tornado.web.StaticFileHandler, {'path': os.path.dirname(os.path.realpath(__file__))+'/ThunderDash/'}),
     ])
 
-def start_wt_server(worker,joy):
-    app = make_app(worker,joy)
+def start_wt_server(aircraft_con):
+    print("Start wt servers")
+    app = make_app(aircraft_con.proxy_8111_worker, aircraft_con.vjoy,aircraft_con)
     app.listen(8888)
+    print("Start Listen")
     tornado.ioloop.IOLoop.current().start()
