f4ff8bf75e52f39b15a7b80219c85162137b1219
xuhao1 <xuhao3e8@gmail.com>
2017-09-26 15:40:52 +0800

    Improve Zoom

--- main.py
+++ main.py
@@ -13,35 +13,48 @@ from pynput.keyboard import Key, Listener
 
 class game_aircraft_control:
     def __init__(self):
-        pygame.init()
-        pygame.joystick.init()
 
-        self.t16000m = None
         self.master = None
         self.cmdpov = None
 
-        for k in range(pygame.joystick.get_count()):
-            if pygame.joystick.Joystick(k).get_name() == "T.16000M":
-                self.t16000m = pygame.joystick.Joystick(k)
-        if self.t16000m is None:
-            print("No t16000m found")
-        else:
-            self.t16000m.init()
-            print("Found Joystick {0}".format(self.t16000m.get_name()))
-
+        self.init_external_joys()
         self.q_base = None
         self.vjoy = pyvjoy.VJoyDevice(1)
         self.proxy_8111_worker = proxy_8111()
 
         self.pov_x_offset = 0
         self.pov_y_offset = 0
-
+        self.zoom = 0
+        self.zoom_base_quat = None
+        self.zoom_base_povx = 0
+        self.zoom_base_povy = 0
+        self.zoom_on_press = False
         try:
             self.master = mavutil.mavlink_connection("COM3", baud="230400")
             print("Link to COM3 with mavlink")
-        except:
-            print("No master Found")
+        except Exception as e:
+            print("No master Found {0}".format(e))
             self.master = None
+
+    def init_external_joys(self):
+        pygame.init()
+        pygame.joystick.init()
+        self.t16000m = None
+        self.twcs = None
+        for k in range(pygame.joystick.get_count()):
+            joystick = pygame.joystick.Joystick(k)
+            if joystick.get_name() == "T.16000M":
+                self.t16000m = pygame.joystick.Joystick(k)
+                self.t16000m.init()
+                print("Found Joystick {0}".format(self.t16000m.get_name()))
+
+            if joystick.get_name() == "TWCS Throttle":
+                self.twcs = joystick
+                self.twcs.init()
+                print("Found Joystick {0}".format(self.twcs.get_name()))
+
+        if self.t16000m is None:
+            print("No t16000m found")
 
     def on_press(self, key):
         if key == Key.f9:
@@ -51,6 +64,21 @@ class game_aircraft_control:
     def set_pov(self, x, y):
         self.vjoy.set_cont_pov(2, toHexCmd(0.1 + y + self.pov_y_offset))
         self.vjoy.set_cont_pov(1, toHexCmd(0.1 + x + self.pov_x_offset))
+
+    def set_zoom(self, zoom):
+        # zoom 0-1
+        self.vjoy.set_axis(pyvjoy.HID_USAGE_SL1, toHexCmd((zoom - 0.5) * 2))
+        return
+
+    def get_POVXYZ_from_quat(self,quat):
+        eul = quat.euler * 180 / 3.14
+        cmd = eul / 180
+        v = quat.transform(np.array([1,0,0]))
+        #print(v)
+        yang = - math.asin(v[2])
+        cmd[1] = yang
+        #print(yang/3.14)
+        return cmd
 
     def update_from_mavlink(self):
         if self.master is None:
@@ -65,37 +93,63 @@ class game_aircraft_control:
                 quat = [msg.q1, msg.q2, msg.q3, msg.q4]
                 self.process_pov_quat(quat)
 
-    def process_pov_quat(self,quat):
+    def process_pov_quat(self, quat):
         q_now = QuaternionBase(attitude=quat)
         if self.q_base is None:
             self.q_base = q_now.inversed
-        q_now = self.q_base.__mul__(q_now)
-        eul = q_now.euler* 180 / 3.14
-        #print(eul)
-        #print(eul)
+        if self.zoom == 0 or self.zoom_base_quat is None:
+            self.zoom_base_quat = q_now.inversed
+
+        cmd = None
+        if self.zoom == 0:
+            q_now = self.q_base.__mul__(q_now)
+            q_now.normalize()
+            cmd = self.get_POVXYZ_from_quat(q_now)
+            cmd[2] = cmd[2] * 10
+            cmd[1] = cmd[1] * 1
+            self.zoom_base_povx = cmd[2]
+            self.zoom_base_povy = cmd[1]
+        else:
+            q_now = self.zoom_base_quat.__mul__(q_now)
+            cmd = self.get_POVXYZ_from_quat(q_now)
+            cmd[2] = cmd[2] * 2.0 + self.zoom_base_povx
+            cmd[1] = cmd[1] * 0.4 + self.zoom_base_povy
+            pass
+
         if self.cmdpov is None:
-            self.cmdpov = eul / 180
+            self.cmdpov = cmd
         else:
             # A naive lowpass filter
-            self.cmdpov = eul / 180 * 0.4 + self.cmdpov * 0.6
-        self.set_pov(-self.cmdpov[2], self.cmdpov[1])
+            self.cmdpov = cmd * 0.4 + self.cmdpov * 0.6
+
+        self.cmdpov[2] = - self.cmdpov[2]
+        #print(self.cmdpov)
+        self.set_pov(self.cmdpov[2], self.cmdpov[1])
 
     def update_from_external_joy(self):
         pygame.event.get()
-        if self.t16000m is None:
+        if self.t16000m is None and self.twcs is None:
             return
-        # print(self.t16000m.get_hat(0))
-        # print(self.t16000m.get_axis(0))
         self.pov_x_offset, self.pov_y_offset = self.t16000m.get_hat(0)
-        self.pov_x_offset = -self.pov_x_offset *0.2
-        self.pov_y_offset = self.pov_y_offset *0.2
+        # if not(self.twcs is None):
+        #    self.pov_x_offset, self.pov_y_offset = self.twcs.get_axis(0),-self.twcs.get_axis(1)
+        self.pov_x_offset = -self.pov_x_offset * 0.4
+        self.pov_y_offset = self.pov_y_offset * 0.2
+        if self.t16000m.get_button(3) > 0:
+            if not self.zoom_on_press:
+                self.zoom = 1 - self.zoom
+            self.zoom_on_press = True
+        else:
+            self.zoom_on_press = False
+
+        self.set_zoom(self.zoom)
 
     def main_thread(self):
         if not (self.master is None):
             print("recieve master")
             self.master.recv()
         while True:
-            #print("refresh")
+            # print("refresh")
             if not (self.master is None):
                 self.update_from_mavlink()
             else:
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import math
+from  math import acos,sqrt
+import numpy as np
 def toHexCmd(cmd):
     #convert -1 to 1 to 0 to 32768
     cmd = math.floor(32768 * (cmd +1) /2)
@@ -7,3 +9,21 @@ def toHexCmd(cmd):
     if cmd > 32768:
         return 32768
     return cmd
+
+def toAxisAngle(quat):
+    qw = quat[0]
+    qx = quat[1]
+    qy = quat[2]
+    qz = quat[3]
+    s = sqrt(1-qw*qw)
+    angle = 2 * acos(qw)
+    if s < 0.001:
+        x = qx
+        y = qy
+        z = qz
+        return np.array([angle * x, angle * y, angle * z])
+    else:
+        x = qx / sqrt(1 - qw * qw)
+        y = qy / sqrt(1 - qw * qw)
+        z = qz / sqrt(1 - qw * qw)
+        return np.array([angle*x,angle*y,angle*z])
